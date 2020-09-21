01c4416e3bb27636d1251402874bd2c4179b5063
Rachit Verma <rachitverma.ea@gmail.com>
2020-09-21 22:57:49 +0530

    Deleted Everything

--- core/assets/fdss.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="fdss" tilewidth="41" tileheight="36" tilecount="12" columns="3">
- <image source="MainGuySpriteSheet.png" width="124" height="144"/>
-</tileset>
