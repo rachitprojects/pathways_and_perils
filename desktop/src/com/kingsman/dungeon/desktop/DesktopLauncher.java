0bede14cf05ba40913473f4e6b8c35c3ac7134bd
Rachit Verma <rachitverma.ea@gmail.com>
2020-09-29 17:09:13 +0530

    Restructured the Directory

---
+++ desktop/src/com/kingsman/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kingsman.dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kingsman.dungeon.Dungeon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dungeon(), config);
+	}
+}
